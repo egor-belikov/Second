b346ef160c42c103874d3578d4edeaa55e62e5a7
egor-belikov <egor-belikov@yandex.ru>
2014-05-05 17:58:35 +0400

    Updated 2-5

--- dsu.cpp
+++ dsu.cpp
@@ -7,28 +7,45 @@ using namespace std;
 vector<int> parent;
 vector<int> rk;
 
-void make_set (int v) {
-	parent[v] = v;
-	rk[v] = 0;
-}
+class dsu
+{
+    vector<int> parent, rank;
+    
+    public:
+    
+    dsu(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            parent.push_back(i);
+        }
+        rk.assign(n, 0);
+    }
 
-int find_set(int v) {
-	if (v == parent[v])
-		return v;
-	return parent[v] = find_set (parent[v]);
-}
 
-void union_sets (int a, int b) {
-	a = find_set (a);
-	b = find_set (b);
-	if (a != b) {
-		if (rk[a] < rk[b])
-			swap (a, b);
-		parent[b] = a;
-		if (rk[a] == rk[b])
-			++rk[a];
-	}
-}
+    void make_set (int v) {
+        parent[v] = v;
+        rk[v] = 0;
+    }
+
+    int find_set(int v) {
+        if (v == parent[v])
+            return v;
+        return parent[v] = find_set (parent[v]);
+    }
+
+    void union_sets (int a, int b) {
+        a = find_set (a);
+        b = find_set (b);
+        if (a != b) {
+            if (rk[a] < rk[b])
+                swap (a, b);
+            parent[b] = a;
+            if (rk[a] == rk[b])
+                ++rk[a];
+        }
+    }
+};
 
 int main(int argc, const char * argv[])
 {
@@ -36,15 +53,7 @@ int main(int argc, const char * argv[])
     cin >> n;
     int commands; //для удобства попросим сразу количество команд. понятно, что для честного онлайн решения поменять потребуется немногое
     cin >> commands;
-    for (int i = 0; i < n; i++)
-    {
-        parent.push_back(0);
-        rk.push_back(0);
-    }
-    for (int i = 0; i < n; i++)
-    {
-        make_set(i);
-    }
+    dsu q(n);
     for (int i = 0; i < commands; i++)
     {
         string spec; //ADD — добавить, CONNECTED — запрос
@@ -52,9 +61,9 @@ int main(int argc, const char * argv[])
         cin >> spec >> x >> y;
         x--; y--;
         if (spec == "ADD")
-            union_sets(x, y);
+            q.union_sets(x, y);
         else
-            if (find_set(x) != find_set(y))
+            if (q.find_set(x) != q.find_set(y))
                 cout << "NO\n";
             else
                 cout << "YES\n";
