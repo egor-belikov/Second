e1690130188dd2085c81510d27aeaddeb9784b3b
egor-belikov <egor-belikov@yandex.ru>
2014-05-25 15:13:50 +0400

    Task 3-2

---
+++ arbitrage.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+#include <vector>
+#include <cfloat>
+#include <math.h>
+#include <algorithm>
+
+using namespace std;
+
+struct edge
+{
+    int a, b;
+    double cost;
+};
+
+int n, m;
+const double INF = DBL_MAX;
+
+void fordbellman(const vector<edge> &e, int v)
+{
+    vector<double> d (n, 0);
+    d[v] = 1;
+    vector<int> p (n, -1);
+    int x;
+    for (int i=0; i<n; ++i) {
+        x = -1;
+        for (int j=0; j<m; ++j)
+            if (d[e[j].a] > 0)
+                if (d[e[j].b] < d[e[j].a] * e[j].cost)
+                {
+                    d[e[j].b] = min (INF, d[e[j].a] * e[j].cost);
+                    p[e[j].b] = e[j].a;
+                    x = e[j].b;
+                }
+
+    }
+    if (x == -1)
+        cout << "No negative cycle from " << v;
+    else
+    {
+        int y = x;
+        for (int i=0; i<n; ++i)
+            y = p[y];
+        
+        vector<int> path;
+        for (int cur=y; ; cur=p[cur]) {
+            path.push_back (cur);
+            if (cur == y && path.size() > 1)  break;
+        }
+        reverse (path.begin(), path.end());
+        
+        cout << "Negative cycle: ";
+        for (size_t i=0; i<path.size(); ++i)
+            cout << path[i] + 1 << ' ';
+        cout << endl;
+    }
+}
+
+void print_cycle(const vector<vector<int> > &r, int x, int y)
+{
+    if (r[x][y] == -1)
+        cout << x + 1 << " ";
+    else
+    {
+        print_cycle(r, x, r[x][y]);
+        print_cycle(r, r[x][y], y);
+    }
+}
+
+void floyd(const vector<vector<double> > &d)
+{
+    vector<vector<double> > a = d;
+    vector<vector<int> > r;
+    for (int i = 0; i < n; i++)
+    {
+        vector<int> t(n, -1);
+        r.push_back(t);
+    }
+    vector<int> cycle;
+    int start;
+    bool cycle_exists = false;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = 0; k < n; k++)
+                if (((a[j][i] > 0) && (a[i][k] > 0)) && (a[j][i] * a[i][k] > a[j][k]))
+                {
+                    a[j][k] = min(INF, a[j][i] * a[i][k]);
+                    r[j][k] = i;
+                    if ((j == k) && (a[j][j] > 1))
+                    {
+                        cycle_exists = true;
+                        start = j;
+                        break;
+                    }
+                }
+            if (cycle_exists)
+                break;
+        }
+        if (cycle_exists)
+            break;
+    }
+    if (!cycle_exists)
+        cout << "No negative cycle\n";
+    else
+    {
+        cout << "Negative cycle: ";
+        print_cycle(r, start, start);
+        cout << start + 1 << endl;
+    }
+        
+    
+}
+int main()
+{
+    vector<vector<double> > a;
+    vector<edge> e;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        vector<double> t;
+        a.push_back(t);
+        for (int j = 0; j < n; j++)
+        {
+            double tt;
+            cin >> tt;
+            a[i].push_back(tt);
+            edge k;
+            k.a = i;
+            k.b = j;
+            k.cost = tt;
+            e.push_back(k);
+            m++;
+        }
+    }
+    fordbellman(e, 0);
+    floyd(a);
+}
+
